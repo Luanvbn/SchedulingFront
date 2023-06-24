cda6198d44ea9ad61cc20faeca9bde848c28c024
Luanvbn <luanvbn@gmail.com>
2023-06-24 19:08:05 -0300

    Decodificando o JWT e pegando a ROLE

--- src/app/SchedullingBarber/service/AuthService.service.ts
+++ src/app/SchedullingBarber/service/AuthService.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { environment } from 'src/environments/environment'
-import { AuthenticationRequest } from '../model/AuthenticationRequest.model'
-import { tap } from 'rxjs'
 
+import { tap } from 'rxjs'
+import { AuthenticationRequest } from '../model/AuthenticationRequest.model'
+import jwtDecode from 'jwt-decode'
 @Injectable({
   providedIn: 'root',
 })
@@ -17,9 +18,22 @@ export class AuthService {
     return this.http.post(this.apiUrl + '/auth/authenticate', authenticationRequest).pipe(
       tap((response: any) => {
         localStorage.setItem('token', response.token)
+        console.log(this.whatAuthority())
         console.log(response.token)
       })
     )
+  }
+
+  getToken() {
+    return localStorage.getItem('token') as any
+  }
+
+  public decodePayloadJWT(): any {
+    try {
+      return jwtDecode(this.getToken())
+    } catch (Error) {
+      return null
+    }
   }
 
   logout(): void {
@@ -29,5 +43,23 @@ export class AuthService {
 
   isAuthenticated(): boolean {
     return this.loggedIn || localStorage.getItem('token') !== null
+  }
+
+  /* Lembrar de passar esse valor do whatAuthority para o Login.component para redirecionar o tipo de login */
+
+  whatAuthority() {
+    const decodedToken = this.decodePayloadJWT()
+
+    if (decodedToken && decodedToken.authorities && decodedToken.authorities.length > 0) {
+      const authorities = decodedToken.authorities
+
+      if (authorities.some((authority) => authority.authority === 'ROLE_ADMIN')) {
+        return 'ROLE_ADMIN'
+      } else if (authorities.some((authority) => authority.authority === 'ROLE_BARBER')) {
+        return 'ROLE_BARBER'
+      }
+    }
+
+    return 'ROLE_CLIENT'
   }
 }
