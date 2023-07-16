60224fed8ee3a03b743b82b2218d726febb41984
Luanvbn <luanvbn@gmail.com>
2023-07-16 19:22:46 -0300

    Adição de warning caso de erro de login

--- src/app/SchedullingBarber/authentication/login/login.component.ts
+++ src/app/SchedullingBarber/authentication/login/login.component.ts
@@ -14,11 +14,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 export class LoginComponent implements OnInit {
   authRequest: AuthenticationRequest = new AuthenticationRequest()
   loginForm: FormGroup
-
+  errorResponse: string | null
   constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.formLogin();
+    this.formLogin()
   }
 
   formLogin() {
@@ -31,25 +31,30 @@ export class LoginComponent implements OnInit {
   public login() {
     if (this.loginForm.valid) {
       this.authRequest = this.loginForm.value
-      this.authService.login(this.authRequest).subscribe((result) => {
-        if (result) {
-          console.log(result)
-          switch (this.authService.whatAuthority()) {
-            case 'ROLE_ADMIN': {
-              this.router.navigate(['admin'])
-              break
-            }
-            case 'ROLE_BARBER': {
-              this.router.navigate(['barber'])
-              break
-            }
-            case 'ROLE_CLIENT': {
-              this.router.navigate(['client'])
-              break
+      this.authService.login(this.authRequest).subscribe(
+        (result) => {
+          if (result) {
+            console.log(result)
+            switch (this.authService.whatAuthority()) {
+              case 'ROLE_ADMIN': {
+                this.router.navigate(['admin'])
+                break
+              }
+              case 'ROLE_BARBER': {
+                this.router.navigate(['barber'])
+                break
+              }
+              case 'ROLE_CLIENT': {
+                this.router.navigate(['client'])
+                break
+              }
             }
           }
+        },
+        (error) => {
+          this.errorResponse = 'Login ou senha incorreta!'
         }
-      })
+      )
     }
   }
 }
