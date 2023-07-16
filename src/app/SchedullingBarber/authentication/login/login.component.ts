6aa5b64523e0b1fefc1cff888a60a6ee259bd895
Luanvbn <luanvbn@gmail.com>
2023-07-16 19:41:35 -0300

    Correção tirando o validatorEmail

--- src/app/SchedullingBarber/authentication/login/login.component.ts
+++ src/app/SchedullingBarber/authentication/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
 
   formLogin() {
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required]],
       password: ['', Validators.required],
     })
   }
